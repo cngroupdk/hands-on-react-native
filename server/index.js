844495d051363d2b7506b572a9417ac3d34fc422
Tomáš Horáček <tomas.horacek@gmail.com>
2015-11-24 11:35:09 +0100

    First version of functional server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-var R  = require('ramda');
+var Lnf = require("lnf");
+var R = require('ramda');
 var Twitter = require('twitter');
+var dateFormat = require('dateformat');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -8,34 +13,74 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-var MAIN_TAG = 'reactjs'
+var MAIN_TAG = 'reactjs';
+var OUTPUT_ROOT = path.join(__dirname, 'output');
 
 var params = {
-  q: '#' + MAIN_TAG + ' #success OR #fail OR #ok OR #super OR #great',
+  q: '#' + MAIN_TAG,
   result_type: 'mixed',
   count: '100',
 };
 
+function writeOutputFile(now, outputFilename, outputFilenameSuffix, outputData) {
+  var outputDir = path.join(OUTPUT_ROOT, dateFormat(now, 'yyyy-mm-dd'), dateFormat(now, 'hh-MM'));
+  var outputJSONPath = path.join(outputDir, outputFilename);
+
+  mkdirp(outputDir, function(error) {
+    if (error) {
+      console.error('Mkdirp Error:', error);
+      return;
+    }
+    var outputJSON = JSON.stringify(outputData, null, 2);
+    fs.writeFile(outputJSONPath, outputJSON, function(error) {
+      if (error) {
+        console.error('WriteFile Error:', error)
+        return;
+      }
+      console.log('-- written:', outputFilename);
+
+      var symlinkPath = path.join(OUTPUT_ROOT, outputFilenameSuffix);
+      Lnf(outputJSONPath, symlinkPath, function(error) {
+        if (error) {
+          console.error('Symlink Error:', error);
+          return;
+        }
+
+        console.log('-- created symlink:', symlinkPath);
+      });
+    });
+  });
+}
+
 function searchFofMatchingStatusses() {
-  client.get('search/tweets', params, function(error, tweets, response){
+  client.get('search/tweets', params, function(error, tweets, response) {
     if (!error) {
       var statuses = tweets.statuses;
-      var processedStatues = R.map((status) => {
+      var processedStatuses = R.map((status) => {
         return {
           _hashtags: R.reject(R.equals(MAIN_TAG), R.map(R.prop('text'), R.path(['entities', 'hashtags'], status))) || [],
-          user: R.pick(['screen_name'], status.user),
+          user: R.pick(['screen_name', 'profile_image_url_https'], status.user),
           text: status.text,
           created_at: status.created_at,
         };
       }, statuses);
-      var out = {
-        'groups': R.groupBy(R.path(['_hashtags', 0]), processedStatues)
+      var selectedProcessedStatuses = R.reject(status => status._hashtags.length < 1, processedStatuses)
+      var groupedStatuses = R.groupBy(R.path(['_hashtags', 0]), selectedProcessedStatuses);
+
+      console.log('---- fetched at:', Date());
+      console.log(JSON.stringify(Object.keys(groupedStatuses), null));
+
+      var now = new Date();
+      var outputFilenameSuffix = 'simple.json'
+      var outputFilename = dateFormat('yyyy-mm-dd-hh-MM-ss') + '-' + outputFilenameSuffix;
+      var outputData = {
+        'groups': groupedStatuses,
+        'filename': outputFilename,
       };
 
-      console.log('----', Date());
-      console.log(JSON.stringify(out, null, 2));
+      writeOutputFile(now, outputFilename, outputFilenameSuffix, outputData);
     } else {
-      console.error('Error: ', error);
+      console.error('Twitter API Error:', error);
     }
   });
 }
