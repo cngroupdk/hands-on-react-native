639aee7b2d44fc60d55322b1a82d18c4dcbcbedc
Tomáš Horáček <tomas.horacek@gmail.com>
2015-11-27 17:28:26 +0100

    Add android version

--- HandsOnReactNative-Workshop/index.android.js
+++ HandsOnReactNative-Workshop/index.android.js
@@ -1,46 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
 var {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var DATA = require('./simple.json');
+var StatusCell = require('./cell');
+
 var HandsOnReactNative = React.createClass({
-  render: function() {
+  getInitialState() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
+
+    return {
+      ds: ds,
+      data: null,
+    };
+  },
+
+  componentDidMount() {
+    this._refreshData();
+  },
+
+  _refreshData() {
+    fetch('http://46.101.203.134/simple.json').then(response => {
+      return response.json();
+    }).then(json => {
+      this.setState({
+        data: json,
+      });
+    });
+  },
+
+  _renderSectionHeader(hashtag) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          Hello React Native
-        </Text>
+      <View style={styles.sectionHeader}>
+        <Text style={styles.sectionHeaderText}>#{hashtag}</Text>
       </View>
     );
-  }
+  },
+
+  _renderCell(status) {
+    return (
+      <Text>{status.text}</Text>
+    );
+  },
+
+  render() {
+    if (!this.state.data) {
+      return (
+        <View style={styles.loadingWrapper}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    var groups = this.state.data.groups;
+    var dataSource = this.state.ds.cloneWithRowsAndSections(groups);
+
+    return (
+      <ListView
+        dataSource={dataSource}
+        renderSectionHeader={(sectionData, hashtag) => this._renderSectionHeader(hashtag)}
+        renderRow={status => <StatusCell status={status} />} />
+    );
+  },
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  loadingWrapper: {
+    paddingTop: 100,
+    alignSelf: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  sectionHeader: {
+    alignSelf: 'stretch',
+    backgroundColor: 'lightgray',
+    padding: 5,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  sectionHeaderText: {
+    fontWeight: 'bold',
   },
 });
-
 AppRegistry.registerComponent('HandsOnReactNative', () => HandsOnReactNative);
